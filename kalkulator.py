3371d037e292643694ae6960f32366f859084564
Ola <aleksandra.zboch@gmail.com>
2017-09-16 15:18:34 +0200

    komentarz w kalkulatorze

--- kalkulator.py
+++ kalkulator.py
@@ -3,4 +3,4 @@ a = input('Podaj liczbę: ')
 b = input('Podaj liczbę: ')
 a, b = float (a), float (b)
 average = (a + b) / 2
-print('Średnia liczb', a, 'oraz', 'to', average)
+print('Średnia liczb', a, 'oraz', 'to', average) # projekt z warszatów
